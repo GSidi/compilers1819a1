3a0381f4fa1b0975420f08fb146b092903f8ef66
GSidi <45037105+GSidi@users.noreply.github.com>
2019-03-11 21:15:49 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,88 +1,71 @@
-"""
-Sample script to test ad-hoc scanning by table drive.
-This accepts a number with optional decimal part [0-9]+(\.[0-9]+)?
+def getchar(words,pos):
+	""" returns char at pos of words, or None if out of bounds """
 
-NOTE: suitable for optional matches
-"""
+	if pos<0 or pos>=len(words): return None
 
-def getchar(text,pos):
-	""" returns char category at position `pos` of `text`,
-	or None if out of bounds """
-	
-	if pos<0 or pos>=len(text): return None
-	
-	c = text[pos]
-	
-	# **Σημείο #3**: Προαιρετικά, προσθέστε τις δικές σας ομαδοποιήσεις
-	
-	if c>='0' and c<='9': return 'DIGIT'	# 0..9 grouped together
-	
-	if c=='.': return 'DOT'	# dot as a category by itself
-	
-	return c	# anything else
+	return words[pos]
 	
 
-
-def scan(text,transitions,accepts):
-	""" scans `text` while transitions exist in
-	'transitions'. After that, if in a state belonging to
-	`accepts`, it returns the corresponding token, else ERROR_TOKEN.
+def scan(text,transition_table,accept_states):
+	""" Scans `text` while transitions exist in 'transition_table'.
+	After that, if in a state belonging to `accept_states`,
+	returns the corresponding token, else ERROR_TOKEN.
 	"""
 	
 	# initial state
 	pos = 0
-	state = 's0'
-	# memory for last seen accepting states
-	last_token = None
-	last_pos = None
-	
+	state = 'q0'
 	
 	while True:
 		
-		c = getchar(text,pos)	# get next char (category)
+		c = getchar(text,pos)	# get next char
 		
-		if state in transitions and c in transitions[state]:
-			state = transitions[state][c]	# set new state
+		if state in transition_table and c in transition_table[state]:
+		
+			state = transition_table[state][c]	# set new state
 			pos += 1	# advance to next char
-			
-			# remember if current state is accepting
-			if state in accepts:
-				last_token = accepts[state]
-				last_pos = pos
 			
 		else:	# no transition found
 
-			if last_token is not None:	# if an accepting state already met
-				return last_token,last_pos
-			
-			# else, no accepting state met yet
+			# check if current state is accepting
+			if state in accept_states:
+				return accept_states[state],pos
+
+			# current state is not accepting
 			return 'ERROR_TOKEN',pos
 			
 	
-# **Σημείο #1**: Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων
-transitions = { 's0': { 'DIGIT':'s1' },
-       			's1': { 'DIGIT':'s1','DOT':'s2' },
-       			's2': { 'DIGIT':'s3' },
-       			's3': { 'DIGIT':'s3' }       
-     		  } 
+# the transition table, as a dictionary
+td = {  'q0':{ '0':'q1','1':'q1','2':'q2','3':'q3','4':'q3','5':'q3','6':'q3','7':'q3','8':'q3','9':'q3' },
+        'q1':{ '0':'q3','1':'q3','2':'q3','3':'q3','4':'q3','5':'q3','6':'q3','7':'q3','8':'q3','9':'q3' },
+        'q2':{ '0':'q3','1':'q3','2':'q3','3':'q3','4':'q7' },
+        'q3':{ ':':'q4','.':'q4' },
+        'q4':{ '1':'q5','2':'q5','3':'q5','4':'q5','5':'q5' },
+        'q5':{ '1':'q6','2':'q6','3':'q6','4':'q6','5':'q6','6':'q6','7':'q6','8':'q6','9':'q6' },
+		'q7':{ '.':'q8',':':'q8'}
+     } 
 
-# **Σημείο #2**: Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής
-accepts = { 's1':'INT_TOKEN',
-       		's3':'FLOAT_TOKEN'	
-     	  }
+# the dictionary of accepting states and their
+# corresponding token
+ad = { 'q6':'TIME_TOKEN',
+	   'q8':'TIME_TOKEN'
+     }
 
 
 # get a string from input
 text = input('give some input>')
 
 # scan text until no more input
-while text:		# i.e. len(text)>0
-	# get next token and position after last char recognized
-	token,pos = scan(text,transitions,accepts)
-	if token=='ERROR_TOKEN':
-		print('unrecognized input at position',pos,'of',text)
-		break
-	print("token:",token,"text:",text[:pos])
-	# new text for next scan
-	text = text[pos:]
+while text:	# that is, while len(text)>0
 	
+	# get next token and position after last char recognized
+	token,position = scan(text,td,ad)
+	
+	if token=='ERROR_TOKEN':
+		print('unrecognized input at pos',position+1,'of',text)
+		break
+	
+	print("token:",token,"string:",text[:position])
+	
+	# remaining text for next scan
+	text = text[position:]
